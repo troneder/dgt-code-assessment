7944fe65cb68f15b93838ba9d263d2edf281852a
troneder <tyrone.chen@kingsway.school.nz>
2025-11-07 09:55:35 +1300

    Update area_perimeter.py
    hopefully no bugs now

--- area_perimeter.py
+++ area_perimeter.py
@@ -7,15 +7,20 @@ while(on):
     try:
         width = float(input("Please tell me the width of the fence that is bigger than 0:  "))
         length = float(input("Please tell me the length of the fence that is bigger than 0: "))
-        
-        print("Area is",width*length)
-        print("Perimeter is",(width*2)+(length*2))
+    
+        if(width or length > 0):
+            print("Area is",width*length)
+            print("Perimeter is",(width*2)+(length*2))
 
-        switch = input("Go again? (enter to restart): ")
-        if(switch == ""):
-            on = True
+            switch = input("Go again? (enter to restart): ")
+            if(switch == ""):
+                on = True
+            else:
+                on = False
         else:
-            on = False
-        
+            print("Please enter a number bigger than 0!")
+
     except ValueError:
         print("This is not a valid number!")
+    
+    
